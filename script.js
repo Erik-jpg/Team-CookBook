06fd39d31bbfe88638469ca631b581ebce506eb7
Erik Stone <spologas@gmail.com>
2021-06-18 15:37:40 -0700

    got save, retrieve, and delete buttons working

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ const form = document.querySelector(".card");
 const form1 = document.querySelector(".card1");
 const h2 = document.querySelector("h2");
 const recipeInput = document.querySelector(".card");
+const getButton = document.querySelector('.is-link');
 // const h1 = document.querySelector('h2');
 // // const personalGreeting = document.querySelector('.personal-greeting');
 
 // // Stop the form from submitting when a button is pressed
 // saveBtn.addEventListener("click", function (e) {
-    
+    function recipeDisplayCheck() {
+        if (localStorage.getItem("recipe")) {
+            let recipe = localStorage.getItem("recipe");
+            form1.textContent = "Your, " + recipe
+            }};
 // });
 //   // run function when the 'Say hello' button is clicked
 saveBtn.addEventListener("click", function (e) {
@@ -22,8 +27,11 @@ saveBtn.addEventListener("click", function (e) {
     //     // personalized greetings and updating the form display
     recipeDisplayCheck();
 });
+//Make another localstorage setItem "users recipes" save an array of user's recipes, can call up array with recipe names. When page loads, populate the array how you want them to see it, set array as buttons, that allow the user to select it (for loop to add buttons)
+//save info of recipe use javascript to show. use for loop to match measurement ingredients 
+
 //   // run function when the 'Forget' button is clicked
-form.addEventListener("click", function () {
+deleteBtn.addEventListener("click", function () {
     //     // Remove the stored name from web storage
     localStorage.removeItem("recipe");
     //     // run nameDisplayCheck() to sort out displaying the
@@ -31,26 +39,12 @@ form.addEventListener("click", function () {
     recipeDisplayCheck();
 });
 //   // define the nameDisplayCheck() function
-function recipeDisplayCheck() {
-    //     // check whether the 'name' data item is stored in web Storage
+getButton.addEventListener("click", function recipeDisplayCheck() {
     if (localStorage.getItem("recipe")) {
-        //       // If it is, display personalized greeting
         let recipe = localStorage.getItem("recipe");
-        h1.textContent = "Your, " + recipe;
-        //       h2.textContent = 'Your recipe is, ' + recipe + 'Does this look acceptable?';
-        //       // hide the 'remember' part of the form and show the 'forget' part
-        //       forgetDiv.style.display = 'block';
-        //       rememberDiv.style.display = 'none';
-        //     } else {
-        //       // if not, display generic greeting
-        //     //   h1.textContent = 'Your recipes are saved ';
-        //     //   h2.textContent = 'You have successfully saved the recipe.';
-        //       // hide the 'forget' part of the form and show the 'remember' part
-        //       form.style.display = 'none';
-        //       form1.style.display = 'block';
-        //     }
-        //   }
+        form1.textContent = "Your, " + recipe;
+
         document.body.onload = recipeDisplayCheck;
         console.log(localStorage.getItem("recipe"));
     }
-}
+});
